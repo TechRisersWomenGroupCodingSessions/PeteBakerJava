0c7c97bfe6ecdffd124b6bc3588f3b4c5815c3f5
lawcia <lawrencianjume@gmail.com>
2024-02-28 19:13:31 +0000

    should bake 0 cakes

--- src/main/java/org/example/Baker.java
+++ src/main/java/org/example/Baker.java
@@ -7,7 +7,12 @@ import java.util.Map;
 public class Baker {
 
     public int getNumberOfCakes(List<RecipeIngredient> recipe, List<Ingredient> cupboard) {
-       return 1;
+        int numberOfCakes = 0;
+
+        for (Ingredient cupboardIngredient : cupboard) {
+            numberOfCakes = cupboardIngredient.countOfIngredient;
+        }
+       return numberOfCakes;
     }
 
 }
--- src/test/java/PeterBakerTest.java
+++ src/test/java/PeterBakerTest.java
@@ -28,16 +28,38 @@ public class PeterBakerTest {
     @Test
     public void shouldBakeOneCakeWhenExactAmountOfSugar() {
         Baker baker = new Baker();
-        RecipeIngredient recipeFlour = new RecipeIngredient("Sugar", 1);
-        Ingredient flour = new Ingredient("Sugar", 1);
+        RecipeIngredient recipeSugar = new RecipeIngredient("Sugar", 1);
+        Ingredient sugar = new Ingredient("Sugar", 200);
         List<Ingredient> cupboard = new ArrayList<Ingredient>();
-        cupboard.add(flour);
+        cupboard.add(sugar);
         List<RecipeIngredient> recipe = new ArrayList<RecipeIngredient>();
-        recipe.add(recipeFlour);
+        recipe.add(recipeSugar);
 
         int numberOfCakes = baker.getNumberOfCakes(recipe, cupboard);
 
         Assert.assertEquals(200, numberOfCakes);
 
+    }
+
+    @Test
+    public void shouldBakeNoCakesWhenNotEnoughFlour() {
+        Baker baker = new Baker();
+        RecipeIngredient recipeSugar = new RecipeIngredient("Sugar", 1);
+        RecipeIngredient recipeFlour = new RecipeIngredient("Flour", 2);
+
+        Ingredient sugar = new Ingredient("Sugar", 1);
+        Ingredient flour = new Ingredient("Flour", 1);
+
+        List<Ingredient> cupboard = new ArrayList<Ingredient>();
+        cupboard.add(sugar);
+        cupboard.add(flour);
+
+        List<RecipeIngredient> recipe = new ArrayList<RecipeIngredient>();
+        recipe.add(recipeSugar);
+        recipe.add(recipeFlour);
+
+        int numberOfCakes = baker.getNumberOfCakes(recipe, cupboard);
+
+        Assert.assertEquals(0, numberOfCakes);
     }
 }

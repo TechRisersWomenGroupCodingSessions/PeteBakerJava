d1c9b96e2aeea70e722adb8f45f6e8ca031db19e
lawcia <lawrencianjume@gmail.com>
2024-02-28 18:48:17 +0000

    write test for flour

--- src/test/java/PeterBakerTest.java
+++ src/test/java/PeterBakerTest.java
@@ -1,11 +1,25 @@
-import org.junit.jupiter.api.Test;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.example.Baker;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PeterBakerTest {
+    @Test
+    public void shouldBakeOneCakeWhenExactAmountOfFlour() {
+        Baker baker = new Baker();
+        RecipeIngredient recipeFlour = new RecipeIngredient("Flour", 1);
+        Ingredient flour = new Ingredient("Flour", 1);
+        List<Ingredient> cupboard = new ArrayList<Ingredient>();
+        cupboard.add(flour);
+        List<RecipeIngredient> recipe = new ArrayList<RecipeIngredient>();
+        recipe.add(recipeFlour);
 
+        Integer numberOfCakes = baker.getNumberOfCakes(recipe, cupboard);
+
+        Assert.assertEquals(1, numberOfCakes);
+
+    }
 }

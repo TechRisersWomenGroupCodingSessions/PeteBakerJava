f1ff60675ee6ca58fc7b80623edd7574636815a1
lawcia <lawrencianjume@gmail.com>
2024-02-28 18:26:19 +0000

    clear last

--- src/main/java/org/example/Baker.java
+++ src/main/java/org/example/Baker.java
@@ -4,17 +4,5 @@ import java.util.Map;
 
 public class Baker {
 
-    public int cakes(Map<String, Integer> cakeRecipe, Map<String, Integer> peterIngredient) {
-        int numberOfCakes = Integer.MAX_VALUE;
 
-        for (Map.Entry<String, Integer> entry : cakeRecipe.entrySet()) {
-            if(!peterIngredient.containsKey(entry.getKey())) {
-                return 0;
-            }
-            else if(peterIngredient.containsKey(entry.getKey()) && peterIngredient.get(entry.getKey())/entry.getValue() < numberOfCakes) {
-                numberOfCakes = peterIngredient.get(entry.getKey())/entry.getValue();
-            }
-        }
-        return numberOfCakes;
-    }
 }

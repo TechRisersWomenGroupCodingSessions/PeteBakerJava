64077723fb3f7a7d46490c336ea53a9cb5d20e92
Ellen Houghton <ellen.a.ash@gmail.com>
2024-03-20 18:25:55 +0000

    add do while loop starter

--- src/main/java/org/example/Baker.java
+++ src/main/java/org/example/Baker.java
@@ -13,22 +13,35 @@ public class Baker {
 
 
         for (RecipeIngredient recipeIngredient : recipe) {
-            for (Ingredient cupboardIngredient : cupboard) {
-                if(recipeIngredient.ingredientType.equals(cupboardIngredient.ingredientType)) {
-                    int numberOfCakes_2 = cupboardIngredient.countOfIngredient/ recipeIngredient.countOfIngredient;
-                    if (numberOfCakes<numberOfCakes_2){
-                        numberOfCakes = numberOfCakes_2;
+            //for (Ingredient cupboardIngredient : cupboard) {
+                // if(recipeIngredient.ingredientType.equals(cupboardIngredient.ingredientType)) {
+                //     int numberOfCakes_2 = cupboardIngredient.countOfIngredient/ recipeIngredient.countOfIngredient;
+                //     if (numberOfCakes<numberOfCakes_2){
+                //         numberOfCakes = numberOfCakes_2;
+                //     }
+                //     else {
+                //         numberOfCakes = 0;
+                //     }
+                //     break;
+                // }
+                // else {
+                //     return 0;
+                // }
+                int counter = 0;
+                var cupboardIngredient = cupboard[counter];
+                do {
+                    numberOfCakes = cupboardIngredient.countOfIngredient/ recipeIngredient.countOfIngredient;
+                } while (counter < cupboard.size()) {
+                    int temp = recipeIngredient.ingredientType.equals(cupboardIngredient.ingredientType);
+                    if (temp < numberOfCakes){
+                        numberOfCakes = temp;
                     }
-                    else {
-                        numberOfCakes = 0;
-                    }
-                    break;
-                }
-                else {
-                    return 0;
-                }
+                    counter++;
+                //}
             }
         }
+
+
 
 
         return numberOfCakes;

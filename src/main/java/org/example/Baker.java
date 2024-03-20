3bfbf6f07f7a89c26275e3874ed740c9baeff615
I337592 <parisanoushazaran@gmail.com>
2024-03-20 18:40:41 +0000

    Baker class updates

--- src/main/java/org/example/Baker.java
+++ src/main/java/org/example/Baker.java
@@ -9,43 +9,25 @@ public class Baker {
     public int getNumberOfCakes(List<RecipeIngredient> recipe, List<Ingredient> cupboard) {
         int numberOfCakes = 0;
 
-        // we're going about this incorrectly because we cannot get past 0 with the current logic
-
 
         for (RecipeIngredient recipeIngredient : recipe) {
-            //for (Ingredient cupboardIngredient : cupboard) {
-                // if(recipeIngredient.ingredientType.equals(cupboardIngredient.ingredientType)) {
-                //     int numberOfCakes_2 = cupboardIngredient.countOfIngredient/ recipeIngredient.countOfIngredient;
-                //     if (numberOfCakes<numberOfCakes_2){
-                //         numberOfCakes = numberOfCakes_2;
-                //     }
-                //     else {
-                //         numberOfCakes = 0;
-                //     }
-                //     break;
-                // }
-                // else {
-                //     return 0;
-                // }
-                int counter = 0;
-                var cupboardIngredient = cupboard[counter];
-                do {
-                    numberOfCakes = cupboardIngredient.countOfIngredient/ recipeIngredient.countOfIngredient;
-                } while (counter < cupboard.size()) {
-                    int temp = recipeIngredient.ingredientType.equals(cupboardIngredient.ingredientType);
-                    if (temp < numberOfCakes){
-                        numberOfCakes = temp;
-                    }
-                    counter++;
-                //}
+
+            int counter = 0;
+            int totalNumberOfCakes = 0;
+            var cupboardIngredient = cupboard.get(counter);
+            do {
+                numberOfCakes = cupboardIngredient.countOfIngredient / recipeIngredient.countOfIngredient;
+                totalNumberOfCakes = totalNumberOfCakes + cupboardIngredient.countOfIngredient / recipeIngredient.countOfIngredient;
+                if (totalNumberOfCakes < numberOfCakes) {
+                    numberOfCakes = totalNumberOfCakes;
+                }
+                counter++;
+            } while (counter < cupboard.size()) {
+
+                }
             }
-        }
-
-
 
 
         return numberOfCakes;
-
-
     }
 }
